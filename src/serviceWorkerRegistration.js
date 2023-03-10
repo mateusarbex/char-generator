943302cca1ffc62d696f18ea2c165392044b1164
Mateus Arbex <51658233+mateusarbex@users.noreply.github.com>
2023-03-10 16:36:33 -0400

    test

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -33,6 +33,24 @@ export function register(config) {
     window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
+      window.applicationCache.addEventListener(
+        "updateready",
+        function (e) {
+          if (
+            window.applicationCache.status ==
+            window.applicationCache.UPDATEREADY
+          ) {
+            // Browser downloaded a new app cache.
+            // eslint-disable-next-line no-restricted-globals
+            if (confirm("A new version of this site is available. Load it?")) {
+              window.location.reload();
+            }
+          } else {
+            // Manifest didn't changed. Nothing new to server.
+          }
+        },
+        false
+      );
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
